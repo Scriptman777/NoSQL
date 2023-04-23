7c4a9a1cea4108464e00058eb2431bd5eef8a267
Scriptman 777 <dave56938@gmail.com>
2023-04-23 08:56:23 +0200

    Basic Replication

---
+++ startRepl.js
@@ -0,0 +1,22 @@
+var config = {
+    "_id": "GarbageReplSet",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "mongo-primary:27017",
+            "priority": 4
+        },
+        {
+            "_id": 2,
+            "host": "mongo-secondary-a:27017",
+            "priority": 3
+        },
+        {
+            "_id": 3,
+            "host": "mongo-secondary-b:27017",
+            "priority": 2
+        }
+    ]
+};
+rs.initiate(config, { force: true });
